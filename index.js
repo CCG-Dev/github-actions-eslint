0bf14155cc329b49f5c9a87a1316ff95457c808a
Reiner Leal <reinerleal@coreconstruction.com>
2020-02-21 07:22:06 -0600

    chore: debugging

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const {
 	GITHUB_WORKSPACE,
 } = process.env;
 
+const [owner, repo] = GITHUB_REPOSITORY.split('/');
+
 async function run() {
 	try {
 		const token = core.getInput('repo-token', { required: true });
-		const [owner, repo] = GITHUB_REPOSITORY.split('/');
 
 		const client = new github.GitHub(token);
 
